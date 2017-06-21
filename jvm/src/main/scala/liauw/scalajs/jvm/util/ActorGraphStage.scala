7eceabbc7b70c25a07afb9ceffcc76f45358fd55
Didier Liauw <didierliauw@gmail.com>
2017-06-21 14:10:23 +0200

    Added ActorGraphStage

---
+++ jvm/src/main/scala/liauw/scalajs/jvm/util/ActorGraphStage.scala
@@ -0,0 +1,51 @@
+package liauw.scalajs.jvm.util
+
+import ActorGraphStage.GraphStageStarted
+import akka.actor.ActorRef
+import akka.http.scaladsl.model.ws.{Message, TextMessage}
+import akka.stream._
+import akka.stream.stage._
+
+object ActorGraphStage {
+  case class GraphStageStarted(graphStageActor: ActorRef)
+}
+
+class ActorGraphStage(val handlerActor: ActorRef) extends GraphStage[FlowShape[Message,Message]] {
+  val in = Inlet[Message]("stage.in")
+  val out = Outlet[Message]("stage.out")
+
+  override val shape = FlowShape.of(in, out)
+  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = {
+    var buffer: Vector[Any] = Vector()
+
+    new GraphStageLogic(shape) {
+      override def preStart() = {
+        val actor = getStageActor { case (ref: ActorRef, message: Any) =>
+          buffer :+= message
+          if(isAvailable(out)) {
+            push(out, TextMessage.Strict(buffer.head.toString))
+            buffer = buffer.tail
+          }
+        }
+        handlerActor ! GraphStageStarted(actor.ref)
+        pull(in)
+      }
+
+      setHandler(in, new InHandler {
+        override def onPush(): Unit = {
+          val message = grab(in)
+          handlerActor ! message.asTextMessage.getStrictText
+          if(!hasBeenPulled(in)) pull(in)
+        }
+      })
+      setHandler(out, new OutHandler {
+        override def onPull(): Unit = {
+          if(buffer.nonEmpty && isAvailable(out)) {
+            push(out, TextMessage.Strict(buffer.head.toString))
+            buffer = buffer.tail
+          }
+        }
+      })
+    }
+  }
+}
